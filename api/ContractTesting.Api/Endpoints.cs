9dae04a49fab6c9d411b661f768f7786fa9838c0
Tomasz Rutkowski <tomasz.rutkowski@rxcorp.net>
2025-08-14 09:58:45 +0200

    feat(api): create products GET endpoint

--- api/ContractTesting.Api/Endpoints.cs
+++ api/ContractTesting.Api/Endpoints.cs
@@ -1,9 +1,30 @@
+using Bogus;
+
 namespace ContractTesting.Api;
 
 public static class Endpoints
 {
-    public static void MapEndpoints(this WebApplication app)
+    public class Product
     {
-        app.MapGet("/health", () => true);
+        public required int Id { get; init; }
+        public required string Name { get; init; }
+        public required string Description { get; init; }
+        public required decimal Price { get; init; }
+    }
+
+    public static void MapEndpoints(this IEndpointRouteBuilder builder)
+    {
+        builder.MapGet("/health", () => true);
+
+        builder.MapGet("/api/products", () =>
+        {
+            var productFaker = new Faker<Product>()
+                .RuleFor(p => p.Id, f => f.IndexFaker + 1)
+                .RuleFor(p => p.Name, f => f.Commerce.ProductName())
+                .RuleFor(p => p.Description, (f, p) => $"Description for {p.Name}")
+                .RuleFor(p => p.Price, f => decimal.Parse(f.Commerce.Price(1, 100)));
+
+            return productFaker.Generate(5).ToArray();
+        });
     }
 }
